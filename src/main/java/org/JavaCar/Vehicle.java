60add655f6fd392559790655cc7b6df80d9870ba
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2025-03-14 16:58:35 +0100

    Update Vehicle.java
    Creamos el constructor y los getters de la clase Abstracta Vehicle

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -10,4 +10,37 @@ public abstract class Vehicle implements Llogable{
     private Roda rodes;
     private String etiquetaAmbiental;   //Etiqueta ambiental del vehiculo
 
+     //Constructor de la clase Vehicle
+     public Vehicle(String matricula, String marca, String model, double preuBase, Motor motor, Roda rodes, String etiquetaAmbiental){
+        this.matricula = matricula;
+        this.marca = marca;
+        this.model = model;
+        this.preuBase = preuBase;
+        this.motor = motor;
+        this.rodes = rodes;
+        this.etiquetaAmbiental = etiquetaAmbiental;
+    }
+
+    //Getters
+    public String getMatricula(){
+        return matricula;
+    }
+    public String getMarca(){
+        return marca;
+    }
+    public String getModel(){
+        return model;
+    }
+    public double getPreuBase(){
+        return preuBase;
+    }
+    public Motor getMotor(){
+        return motor;
+    }
+    public Roda getRodes(){
+        return rodes;
+    }
+    public String getEtiquetaAmbiental(){
+        return etiquetaAmbiental;
+    }   
 }
