68e02acb8e4044d21465f40ad29569aede6cad4b
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2025-03-15 23:49:04 +0100

    Update class Moto

--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -1,5 +1,19 @@
 package org.JavaCar;
 
-public class Moto {
+public class Moto extends Vehicle {
+    public int cilindrada;
+
+    public Moto(String matricula, String marca, String model, double preuBase, Motor motor, Roda rodes, String etiquetaAmbiental, int cilindrada){
+        super(matricula, marca, model, preuBase, motor, rodes, etiquetaAmbiental);
+        this.cilindrada = cilindrada;
+    }
     
+    @Override
+    public double calcularPrecio(int numDies) {
+        double preu = getPreuBase() * numDies;
+        if (cilindrada > 500) {
+            preu += 5;
+        }
+        return preu;
+    }
 }
