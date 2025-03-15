75c65aebdcd37df0e50aa114f7acd35f92d2090a
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2025-03-15 23:40:59 +0100

    Update class Cotxe

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -1,5 +1,15 @@
 package org.JavaCar;
 
-public class Cotxe {
+public class Cotxe extends Vehicle {
+    private int nombrePlaces;
+
+    public Cotxe(String matricula, String marca, String model, double preuBase, Motor motor, Roda rodes, String etiquetaAmbiental, int nombrePlaces){
+        super(matricula, marca, model, preuBase, motor, rodes, etiquetaAmbiental);
+        this.nombrePlaces = nombrePlaces;
+    }
     
+    @Override
+    public double calcularPrecio(int numDies){
+        return getPreuBase() * numDies;
+    }
 }
