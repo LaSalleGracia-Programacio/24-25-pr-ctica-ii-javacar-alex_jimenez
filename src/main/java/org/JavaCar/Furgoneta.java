960bbb8865f5308a69c3fbf13e48a834bd2d066d
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2025-03-20 22:22:19 +0100

    summary required

--- src/main/java/org/JavaCar/Furgoneta.java
+++ src/main/java/org/JavaCar/Furgoneta.java
@@ -1,5 +1,16 @@
 package org.JavaCar;
 
-public class Furgoneta {
-    
+//Clase Furgoneta heredada de vehicle que implemnta la interfaz llogable
+public class Furgoneta extends Vehicle implements Llogable {
+    private int capacitatCarga;
+
+    public Furgoneta(String matricula, String marca, String model, double preuBase, Motor motor, Roda rodes, String etiquetaAmbiental, int capacitatCarga){
+        super(matricula, marca, model, preuBase, motor, rodes, etiquetaAmbiental);
+        this.capacitatCarga = capacitatCarga;
+    }
+
+    @Override
+    public double calcularPrecio(int numDies){
+        return getPreuBase() * numDies + capacitatCarga * 10;
+    }
 }
