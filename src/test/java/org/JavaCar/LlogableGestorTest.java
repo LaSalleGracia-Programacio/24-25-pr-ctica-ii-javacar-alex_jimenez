80286849bdb2a9152ab66b36077dfc11a1c37c6d
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2025-03-21 01:21:02 +0100

    Update and Fix LlogableGestorTest

--- src/test/java/org/JavaCar/LlogableGestorTest.java
+++ src/test/java/org/JavaCar/LlogableGestorTest.java
@@ -11,15 +11,15 @@ import java.util.List;
 public class LlogableGestorTest {
     @Test
     public void testLlogableImplementacio() {
-        Vehicle cotxe = new Cotxe("1234ABC", "Toyota", "Corolla", 30, 5, new Motor("Gasolina", 120), new Roda[]{new Roda("Michelin", 17), new Roda("Michelin", 17), new Roda("Michelin", 17), new Roda("Michelin", 17)});
+        Vehicle cotxe = new Cotxe("1234ABC", "Toyota", "Corolla", 30.0, new Motor("Gasolina", 120), new Roda("Michelin", 17), "ECO", 5);
         assertTrue(cotxe instanceof Llogable);
     }
     
     @Test
     public void testCalcularIngressosTotals() {
         List<Vehicle> vehicles = new ArrayList<>();
-        vehicles.add(new Cotxe("1234ABC", "Toyota", "Corolla", 30, 5, new Motor("Gasolina", 120), new Roda[]{new Roda("Michelin", 17), new Roda("Michelin", 17), new Roda("Michelin", 17), new Roda("Michelin", 17)}));
-        vehicles.add(new Moto("5678DEF", "Yamaha", "R6", 25, 600, new Motor("Gasolina", 80), new Roda[]{new Roda("Pirelli", 16), new Roda("Pirelli", 16)}));
+        vehicles.add(new Cotxe("1234ABC", "Toyota", "Corolla", 30.0, new Motor("Gasolina", 120), new Roda("Michelin", 17), "ECO", 5));
+        vehicles.add(new Moto("5678DEF", "Yamaha", "R6", 25.0, new Motor("Gasolina", 80), new Roda("Pirelli", 16), "ECO", 600));
         assertEquals(180, GestorLloguers.calcularIngressosTotals(vehicles, 3), 0.01);
     }
 }
