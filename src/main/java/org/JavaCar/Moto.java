4e637310b5272f55101e3f64a34a4ee487b5bc08
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2025-03-21 01:25:06 +0100

    Update ClassMoto

--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -1,18 +1,22 @@
 package org.JavaCar;
 
-public class Moto extends Vehicle {
-    public int cilindrada;
+public class Moto extends Vehicle implements Llogable {
+    private int cilindrada;
 
-    public Moto(String matricula, String marca, String model, double preuBase, Motor motor, Roda rodes, String etiquetaAmbiental, int cilindrada){
+    public Moto(String matricula, String marca, String model, double preuBase, Motor motor, Roda rodes, String etiquetaAmbiental, int cilindrada) {
         super(matricula, marca, model, preuBase, motor, rodes, etiquetaAmbiental);
         this.cilindrada = cilindrada;
     }
-    
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
     @Override
-    public double calcularPrecio(int numDies) {
-        double preu = getPreuBase() * numDies;
-        if (cilindrada > 500) {
-            preu += 5;
+    public double calcularPrecio(int dies) {
+        double preu = getPreuBase() * dies;
+        if (cilindrada >= 500) {
+            preu += 5 * dies;
         }
         return preu;
     }
