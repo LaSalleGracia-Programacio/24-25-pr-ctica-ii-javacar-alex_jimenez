a5ca6cb1ba51110ea059b7359de4b2faf45b5954
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2025-04-04 12:47:28 +0200

    Update Main.java
    Update comentarios

--- src/main/java/org/JavaCar/Main.java
+++ src/main/java/org/JavaCar/Main.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;         //Importamos la clase ArrayList
 
 public class Main {
     public static void main(String[] args) {
-        //Creamos los vehiculos Coche, Moto y Furgoneta con los motores y ruedas
-        Motor motorCoche = new Motor("Gasolina", 100);
-        Roda rodesCoche = new Roda("Michelin", 17);
+        //Creamos las clases de los vehiculos Coche, Moto y Furgoneta con los motores y ruedas 
+        Motor motorCoche = new Motor("Gasolina", 100);      //Instancia de motor
+        Roda rodesCoche = new Roda("Michelin", 17);         //Instancia de RUeda
 
         Motor motorMoto = new Motor("Gasolina", 80);
         Roda rodesMoto = new Roda("Pirelli",14);
@@ -15,14 +15,14 @@ public class Main {
         Motor motorFurgoneta = new Motor("Gasolina", 120);
         Roda rodesFurgoneta = new Roda("Michelin", 17);
         
-        //Creamos las instancias de los vehiculos
+        //Creamos las instancias de los vehiculos (objeto creado a partir de una clase)
         Cotxe cotxe = new Cotxe("1234ABCD", "Toyota", "Corolla", 10000, motorCoche, rodesCoche, "ECO", 5);
         Moto moto = new Moto("2341FGDE", "Honda", "CBR1000RR", 15000, motorMoto, rodesMoto, "ECO", 1000);
         Furgoneta furgoneta = new Furgoneta("3456HIJK", "Mercedes", "Sprinter", 20000, 1000, motorFurgoneta, rodesFurgoneta, "ECO");
 
         //Creamos la lista de vehiculos
         List<Vehicle> vehicles = new ArrayList<>();
-        vehicles.add(cotxe);
+        vehicles.add(cotxe);         //Cotxe es un vehicle, herencia y interfaz
         vehicles.add(moto);
         vehicles.add(furgoneta);
 
