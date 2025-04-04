acbb7008e692d0f0396906f4e62eaed059585999
Alex Jiménez <alex.jimenez.estudios@gmail.com>
2025-04-04 11:15:30 +0200

    MEGA ARREGLO
    Tenia duplicado la clase GestorLlogers, he arreglado el test porque estaba con el nonbre incorrecto y fallos derivados

--- src/test/java/org/JavaCar/LlogableGestorTest.java
+++ src/test/java/org/JavaCar/LlogableGestorTest.java
@@ -3,7 +3,7 @@ package org.JavaCar;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-// Classe de testos per Llogable i GestorLloguers
+// Classe de testos per Llogable i GestorLlogers
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +20,6 @@ public class LlogableGestorTest {
         List<Vehicle> vehicles = new ArrayList<>();
         vehicles.add(new Cotxe("1234ABC", "Toyota", "Corolla", 30.0, new Motor("Gasolina", 120), new Roda("Michelin", 17), "ECO", 5));
         vehicles.add(new Moto("5678DEF", "Yamaha", "R6", 25.0, new Motor("Gasolina", 80), new Roda("Pirelli", 16), "ECO", 600));
-        assertEquals(180, GestorLloguers.calcularIngressosTotals(vehicles, 3), 0.01);
+        assertEquals(180, GestorLlogers.calcularIngressosTotals(vehicles, 3), 0.01);
     }
 }
